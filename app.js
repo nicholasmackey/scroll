21aa6ed0c5884b5818c555f2cab29391034a6376
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-21 18:25:49 -0600

    feat: adds/removes enable button

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
 
 function observerCallback(payload) {
-    console.log(payload);
+    if (payload[0].intersectionRatio) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
 }
 
 const observer = new IntersectionObserver(observerCallback)
 
-observer.observe(watch);
+observer.observe(terms.lastElementChild);
