9868099a50eb0b26d956f56cf35a0539f686a224
Nicholas Mackey <59402742+nikmackey@users.noreply.github.com>
2021-01-21 18:30:37 -0600

    feat: sets observer threshholds

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const button = document.querySelector('.accept');
 function observerCallback(payload) {
     if (payload[0].intersectionRatio) {
         button.disabled = false;
+    
     } else {
         button.disabled = true;
     }
 }
 
-const observer = new IntersectionObserver(observerCallback)
+const observer = new IntersectionObserver(observerCallback, {
+    root: terms,
+    threshold: 1,
+});
 
 observer.observe(terms.lastElementChild);
